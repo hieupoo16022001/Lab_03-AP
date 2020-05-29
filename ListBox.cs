ec2264bd91cfee3b379ac03304f510f4bd1627df
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2020-05-29 13:08:03 +0700

    Lap03

---
+++ ListBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03
+{
+    class ListBox : Window
+    {
+        public ListBox(int top, int left, string contents):base(top, left)
+        {
+            listBoxContents = contents;
+        }
+        public override void DrawWindow()
+        {
+            base.DrawWindow();
+            Console.WriteLine("Write string to the list:{0}",listBoxContents);
+        }
+        private string listBoxContents;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car myCar;
+            System.Console.WriteLine("Creating a Car object and assing" + "its memory location to myCar");
+            myCar = new Car();
+
+            myCar.make = "Toyota";
+            myCar.model = "MR2";
+            myCar.color = "Black";
+            myCar.yearBuilt = 1995;
+
+            System.Console.WriteLine("myCar details:");
+            System.Console.WriteLine("myCar.make = " + myCar.make);
+            System.Console.WriteLine("myCar.model =" + myCar.model);
+            System.Console.WriteLine("myCar.color =" + myCar.color);
+            System.Console.WriteLine("myCar.yearBuilt =" + myCar.yearBuilt);
+
+            myCar.Start();
+            myCar.Stop();
+
+            System.Console.WriteLine("Creating another Car object and" + "assing its memory location to redPorche");
+            Car redPorsche = new Car();
+            redPorsche.make = "Porsche";
+            redPorsche.model = "Boxter";
+            redPorsche.color = "Red";
+            redPorsche.yearBuilt = 2000;
+            System.Console.WriteLine("redPorsche is a " + redPorsche.model);
+            System.Console.WriteLine("Assigning redPorsche to myCar");
+            myCar = redPorsche;
+            System.Console.WriteLine("myCar details:");
+            System.Console.WriteLine("myCar.make =" + myCar.make);
+            System.Console.WriteLine("myCar.model =" + myCar.model);
+            System.Console.WriteLine("myCar.color =" + myCar.color);
+            System.Console.WriteLine("myCar.yearBuilt =" + myCar.yearBuilt);
+
+            myCar = null;
+            Console.ReadLine();
+        
+
+        }
+    }
+}
